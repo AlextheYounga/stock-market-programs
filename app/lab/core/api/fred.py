b2f5aca2f0aa2c8b8426346e237f464b08ef9a08
alextheyounger <alex@hazlitt.tech>
2021-09-02 18:34:39 -0400

    Changing date

--- app/lab/core/api/fred.py
+++ app/lab/core/api/fred.py
@@ -111,10 +111,12 @@ class Fred():
                 return 'Up'
 
         twit = Tweet()
+        newdate = datetime.datetime.strptime(new['date'], '%Y-%m-%d').strftime('%b %d')
+        lastdate = datetime.datetime.strptime(last['date'], '%Y-%m-%d').strftime('%b %d')
         diff = str(int(float(new['value']) - float(last['value'])))
-        headline = f"LATEST FED BALANCE SHEET NUMBER {new['date']}:"
+        headline = f"LATEST FED BALANCE SHEET NUMBER {newdate}:"
         value = self.format_number(new['value'])
-        body = f"{preposition(int(diff))} {self.format_number(diff)} compared to last number on {last['date']}"
+        body = f"{preposition(int(diff))} {self.format_number(diff)} compared to last number on {lastdate}"
         tweet_data = [
             headline,
             value,
